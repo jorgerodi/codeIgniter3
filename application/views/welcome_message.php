dc318752f894c6c39299cb982063f88c442c6475
EdgarRodi <sistemas@rodicontrol.com>
2023-12-17 21:41:28 -0600

    Primer commit en la rama main

---
+++ application/views/welcome_message.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Curso code CodeIgniter</title>
+
+	<style>
+		
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Hola  mundo  vamos  por mas retos</h1>
+</div>
+
+</body>
+</html>
